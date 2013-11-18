daa7e38115d1bb39b27966b6cc173af37549f84b
thomasgaillard <thomas.gaillard@me.com>
2013-11-18 16:46:51 +0100

    Verification on columns : check

--- src/Reversi.java
+++ src/Reversi.java
@@ -4,6 +4,15 @@ public class Reversi {
 		char activePlayer = getTurn(input);
 		char passivePlayer = activePlayer == 'B' ? 'W' : 'B';
 		
+		input = input.substring(0, input.length()-1);
+		String verifiedLines = verifyLegalMoves(input, activePlayer, passivePlayer);
+ 		String columnsTranspose = array2str(transpose(str2array(verifiedLines)));
+		String verifiedColumnsTranspose = verifyLegalMoves(columnsTranspose, activePlayer, passivePlayer);
+		String verifiedColumns = array2str(transpose(str2array(verifiedColumnsTranspose)));
+		return verifiedColumns+"\n"+activePlayer;
+	}
+
+	private String verifyLegalMoves(String input, char activePlayer, char passivePlayer) {
 		String output  = input.replaceAll(activePlayer+"("+passivePlayer+"+)\\.", activePlayer+"$10");
 		output  = output.replaceAll("\\.("+passivePlayer+"+)"+activePlayer, "0$1"+activePlayer);
 		return output;
@@ -13,25 +22,31 @@ public class Reversi {
 		return input.charAt(input.length() - 1);
 	}
 	
-	public String[][] string2Matrix(){
-		String[][] matrix = new String[][] { { "1", "2", "3", "4" }, { "5", "6", "7", "8" }, { "9", "10", "11", "12" } };
-		return matrix;
+	public String[] str2array(String str){
+		return str.split("\n");
 	}
 	
-	public String[][] matrix2String(){
-		String[][] matrix = new String[][] { { "1", "2", "3", "4" }, { "5", "6", "7", "8" }, { "9", "10", "11", "12" } };
-		return matrix;
+	public String array2str(String[] array){
+		String ret  = "";
+		for(int x= 0; x < array.length; x++){
+			ret += array[x] ;
+			if(x+1 < array.length){
+				ret += "\n";
+				
+			}
+		}
+		return ret;
 	}
 	
-	public String[][] transpose(String[][] matrix) {
-		int width = matrix.length;
-		int height = matrix[0].length;
-		String[][] transpose = new String[height][width];
-		  for (int x = 0; x < width; x++) {
-		    for (int y = 0; y < height; y++) {
-		    	transpose[y][x] = matrix[x][y];
-		    }
-		  }
-		  return transpose;
+	public String[] transpose(String[] array){
+		System.out.println(array[0].length());
+		String ret[] = new String[array[0].length()];
+		for(int x= 0; x < array[0].length(); x++){
+			ret[x] = "";
+			for(int y= 0; y < array.length; y++){
+				ret[x] += array[y].charAt(x) + "";
+			}
+		}
+		return ret;
 	}
 }
